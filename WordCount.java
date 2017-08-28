1e1fd53735294d2f643f18ed1ef6759e081cd3dc
Alexandre Conti Mestre <alexandre.conti.mestre@gmail.com>
2017-08-28 17:47:33 -0300

    Novo mapper e reducer

--- WordCount.java
+++ WordCount.java
@@ -11,6 +11,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import jdk.nashorn.internal.runtime.Context;
+
 public class WordCount {
     
     public static class TokenizerMapper
@@ -22,35 +24,84 @@ public class WordCount {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String aux = value.toString();
             String [] values = aux.split(",");
+            //System.out.println(values[0]);
             context.write(new Text(values[0]), new Text(values[1]+";"+values[2]));
         }
     }
     
-    public static class IntSumReducer
+    public static class UserRatingReducer
     extends Reducer<Text,Text,Text,Text> {
-        private IntWritable result = new IntWritable();
-        
         public void reduce(Text key, Iterable<Text> values,
                            Context context
                            ) throws IOException, InterruptedException {
-            System.out.println(key);
-            String text = new String();
+            String text = "";
             for (Text val : values) {
                 text = text + "|" + val.toString();
-                System.out.println(val);
-                System.out.println();
             }
             context.write(key, new Text(text));
         }
     }
+
+    public static class CombinationMapper
+    extends Mapper<Object, Text, Text, Text>{
+        
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String aux = value.toString();
+            String movies_line = aux.split("||")[1];
+            String [] movies = movies_line.split("|");
+
+            for (int i=0; i<movies.length; i++) {
+                for (int j=i; i<movies.length; i++) {
+                    if (movies[i].compareTo(movies[j]) <= 0) {
+                        String [] movie1 = movies[i].split(";");
+                        String [] movie2 = movies[j].split(";"); 
+                    } else {
+                        String [] movie2 = movies[i].split(";");
+                        String [] movie1 = movies[j].split(";");
+                    }
+                    context.write((movie1[0] + ";" + movie2[0]),(movie1[1] + ";" + movie2[1]));
+                }
+            }
+
+        }
+    }
+
+    public static class FinalReducer
+    extends Reducer<Text,Text,Text,Text> {
+        public void reduce(Text key, Iterable<Text> values,Context context) throws IOException, InterruptedException {
+            float sum_xx = 0.0, sum_xy = 0.0, sum_yy = 0.0, sum_x = 0.0, sum_y = 0.0;
+            int n = 0;
+
+            String [] moviePair = key.toString().split(";");
+
+            for (Text rating : values) {
+                float r1 = Float.parseFloat(rating.toString().split(";")[0]);
+                float r2 = Float.parseFloat(rating.toString().split(";")[1]);
+
+                sum_xx += r1 * r1;
+                sum_yy += r2 * r2;
+                sum_xy += r1 * r2;
+                sum_y += r2;
+                sum_x += r1;
+                n += 1;
+            }
+
+            float correlation = (n*sum_xy - sum_x*sum_y)/(Math.sqrt(n*sum_xx - (sum_x*sum_x)) * Math.sqrt(n*sum_yy - (sum_y*sum_y)));
+            
+            context.write(key, new Text(correlation));
+        }
+    }
+
+
+
     
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setCombinerClass(UserRatingReducer.class);
+        job.setReducerClass(UserRatingReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
